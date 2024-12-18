2ea6921d5e4313be4ed38eb3d39aef33710f8303
oleg <User@DESKTOP-JLMGOG7>
2024-12-18 14:32:47 +0200

    Add project files.

---
+++ ASP2/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Models;
+using System.Collections.Generic;
+using ASP2.Models;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static List<Employee> employees = new List<Employee>();
+
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employee.Id = employees.Count + 1;
+                employees.Add(employee);
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+    }
+}
---
+++ ASP2/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ім'я є обов'язковим")]
+        public string Name { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Зарплата має бути більшою за 0")]
+        public decimal Salary { get; set; }
+
+        [Required(ErrorMessage = "Посада є обов'язковою")]
+        public string Position { get; set; }
+    }
+}
